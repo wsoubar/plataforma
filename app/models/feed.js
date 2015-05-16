a1f75ca0f78d54e84fe8eafabeac212f636de53c
wsoubar <wsoubar@gmail.com>
2015-05-15 22:05:11 -0300

    ajustes

--- app/models/feed.js
+++ app/models/feed.js
@@ -4,9 +4,7 @@ var Schema          = mongoose.Schema;
 var feedSchema   = new Schema({
 	texto: String,
 	tipo: {type: String, default: 'comentario'},
-	usuario: { 
-		type: Schema.Types.ObjectId, ref: 'Usuario' 
-	},
+	usuario: { type: Schema.Types.ObjectId, ref: 'Usuario' },
 	data: {type: Date, default: Date.now}
 }, { collection: 'feed' });
 
