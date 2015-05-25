d99e29f667b10dc9f643fb3fd61f1406b05f0a01
Wagner Barbosa <wsoubar@gmail.com>
2015-05-25 17:58:18 -0300

    imports dos scripts no index.html

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,7 @@
 // public/js/app.js
 (function(){
 
-	var app = angular.module('plataformaApp', ['ngStorage']);
-
-
+	var app = angular.module('plataformaApp', ['plataformaApp-services', 'plataformaApp-filters', 'plataformaApp-controllers']);
 
 })();
 
